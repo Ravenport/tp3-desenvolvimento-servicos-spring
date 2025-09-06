ea4c753d2f0c4ffb54ae9ee6f790994897e48fb3
unknown <vp22756@gmail.com>
2025-09-06 16:30:55 -0300

    first commit

---
+++ src/main/java/infnet/ds/tp3/sistemainterno/SistemainternoApplication.java
@@ -0,0 +1,13 @@
+package infnet.ds.tp3.sistemainterno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemainternoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemainternoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/infnet/ds/tp3/sistemainterno/SistemainternoApplicationTests.java
@@ -0,0 +1,13 @@
+package infnet.ds.tp3.sistemainterno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemainternoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
